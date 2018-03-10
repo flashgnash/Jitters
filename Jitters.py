efff24f0b9974cce04356b0f9f6068f708d1b540
flashgnash <12749214+flashgnash@users.noreply.github.com>
2018-03-10 11:43:22 +0000

    Update Jitters.py
    Updated to allow the custom menu bar on linux, added themes, added the base for a repeater toggle

--- Jitters.py
+++ Jitters.py
@@ -1,26 +1,33 @@
-#JitterKeys
+#Jitters (Taskinator V3)
 import sys,tkinter,keyboard
 from tkinter import *
 from tkinter import ttk
 
-ThemeSelected = "light"
+ThemeSelected = "Light"
 modifier_start = "shift"
 modifier_list = "shift","ctrl","tab","alt"
-available_themes = "dark","light"
+available_themes = "Dark","Light","Blue","Snow","XP"
 
 def close(window):
     window.destroy()
     quit()
 
 def style(theme):
-    if theme == "light":
-        return {"BG":"darkgray","TCol":"orange","MTCol":"black","Title":"Jitters [light]","other":"gray"}              
-    else:
-        return {"BG":"darkgray","TCol":"black","MTCol":"white","Title":"Jitters [dark]","other":"indigo"}
-    
+    if theme == "Light":
+        return {"BG":"darkgray","TCol":"orange","MTCol":"black","Title":"Jitters","other":"gray"}
+    elif theme == "Dark":
+        return {"BG":"#222222","TCol":"black","MTCol":"white","Title":"Jitters","other":"#333333"}
+    elif theme == "Blue":
+        return {"BG":"darkblue","TCol":"black","MTCol":"white","Title":"Jitters","other":"indigo"}
+    elif theme == "Snow":
+        return {"BG":"#EEEEEE","TCol":"#AAAAAA","MTCol":"#000000","Title":"Jitters","other":"#FFFFFF"}
+    elif theme == "XP":
+        return {"BG":"#EEEECC","TCol":"blue","MTCol":"black","Title":"Jitters","other":"#FFFFDD"}
+
+
 root = Tk()
 root.update()
-    
+
 
 modifier = StringVar(root)
 modifier.set(modifier_start)
@@ -31,29 +38,28 @@ Theme = style(ThemeSelected)
 root.title(Theme["Title"])
 root.configure(background=Theme["BG"])
 
-if sys.platform.find("win") != -1: #Custom menu bar on windows
-    root.attributes("-toolwindow",0,"-topmost",1,"-alpha",0.8)
-    root.overrideredirect(1)
+root.attributes("-type",'splash',"-topmost",1,"-alpha",0.8)
+root.geometry("450x360")
 
-    def move(event):
-        root.geometry('+'+str(event.x_root - 150)+'+'+str(event.y_root))
+def move(event):
+    root.geometry('+'+str(event.x_root - 150)+'+'+str(event.y_root))
 
-    TBar = Frame(root,relief=RAISED)
-    
-    L1 = Label(master=TBar,relief="flat")
-    L1.pack(side="left",fill=Y)    
+TBar = Frame(root,relief=RAISED)
 
-    ExitButton = Button(master=TBar,bg="red",relief=FLAT,width=1,command=lambda: close(root))
-    ExitButton.pack(side=RIGHT)
+L1 = Label(master=TBar,relief="flat")
+L1.pack(side="left",fill=Y)
 
-    TBar.pack(fill=X)
+ExitButton = Button(master=TBar,bg="red",relief=FLAT,width=1,command=lambda: close(root))
+ExitButton.pack(side=RIGHT)
 
-    TBar.bind('<B1-Motion>',move)
-    
+TBar.pack(fill=X)
+
+TBar.bind('<B1-Motion>',move)
+
 L2 = Label(text="Jitters",font = ('Courier', 50))
 L2.pack(pady=10)
 
-KeyLabel = Label(text="Key combo to toggle the auto keypressing")
+KeyLabel = Label(text="Key combo to trigger the macro")
 KeyLabel.pack()
 
 
@@ -71,8 +77,18 @@ ThePlus.pack(side="left")
 E1 = Entry(master=entrybox,width=1,font = ('Courier', 20))
 E1.pack(side="left",pady=5)
 
-L3 = Label(text="Text to be autotyped (use /// for enter)")
-L3.pack(pady=20)
+CbFrame = Frame()
+CbFrame.pack()
+
+toggle_mode = IntVar()
+Cb1 = Checkbutton(master=CbFrame,variable=toggle_mode)
+Cb1.pack(padx=20,side="left")
+
+CBLabel = Label(master=CbFrame,text="Toggle mode (repeat)")
+CBLabel.pack(side="left")
+
+L3 = Label(text="Text to be typed (use /// for enter)")
+L3.pack(pady=10)
 
 E2 = Entry(width=20,font = ('Courier', 20),)
 E2.pack(side="top",pady=5)
@@ -84,7 +100,11 @@ def makeahotkeynowplease():
     except:
         print("Hotkey ["+Hotkey+"] created for the first time")
     TextToType = E2.get().replace("///","\n")
-    keyboard.add_hotkey(Hotkey,lambda: keyboard.write(TextToType))
+    if toggle_mode.get() == 1:
+        print("Test")
+    else:
+        keyboard.add_hotkey(Hotkey,lambda: keyboard.write(TextToType))
+        print(toggle_mode)
 
 
 
@@ -95,7 +115,7 @@ B1 = Button(master=BottomFrame,text="Apply",width=7,command=makeahotkeynowplease
 B1.pack(pady=3)
 
 ThemeSelector = StringVar()
-ThemeSelector.set("light")
+ThemeSelector.set("Light")
 ThemeDropDown = OptionMenu(BottomFrame,ThemeSelector,*available_themes)
 ThemeDropDown.pack(side="left")
 '''
@@ -108,12 +128,10 @@ def keyrelease(key):
 
 keyboard.on_release(keyrelease)
 '''
-root.geometry("450x350")
 def applyTheme(idontcare,about,thesevariables):
     Theme = style(ThemeSelector.get())
-    if sys.platform.find("win") != -1:
-        TBar.configure(bg=Theme["TCol"])
-        L1.configure(text=Theme["Title"],background=Theme["TCol"],fg=Theme["MTCol"])
+    TBar.configure(bg=Theme["TCol"])
+    L1.configure(text=Theme["Title"],background=Theme["TCol"],fg=Theme["MTCol"])
     root.title(Theme["Title"])
     root.configure(bg=Theme["BG"])
     L2.configure(background=Theme["other"],fg=Theme["MTCol"])
@@ -125,8 +143,11 @@ def applyTheme(idontcare,about,thesevariables):
     B1.configure(bg=Theme["other"],fg=Theme["MTCol"])
     BottomFrame.configure(bg=Theme["BG"])
     ThemeDropDown.configure(bg=Theme["BG"],fg=Theme["MTCol"])
-    
-    
+    Cb1.configure(bg=Theme["BG"],fg=Theme["MTCol"])
+    CbFrame.configure(bg=Theme["BG"])
+    CBLabel.configure(bg=Theme["BG"],fg=Theme["MTCol"])
+
+
 applyTheme("these","are","useless")
 
 ThemeSelector.trace("w", applyTheme)
